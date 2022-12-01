c08f4879dcd5f6e425aedf108d66bb424eea2cbc
scalychimp <scalychimp@gmail.com>
2022-12-01 16:49:56 +0000

    ditch emacsclient
    doom emacs is pretty quick to startup so it just isn't worth it.

--- fish/functions/emx.fish
+++
@@ -1,3 +0,0 @@
-function emx --wraps=emacsclient\ -cna\ \'\' --description alias\ emx=emacsclient\ -cna\ \'\'
-  emacsclient -cna '' $argv; 
-end
--- fish/functions/tmx.fish
+++
@@ -1,3 +0,0 @@
-function tmx --wraps='emacsclient -t' --description 'alias tmx=emacsclient -t'
-  emacsclient -tca "" $argv;
-end
